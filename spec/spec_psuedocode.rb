aabf9fc63a87585c59d6c9d376f69939d41196f6
Allie Howard <alliemhow@gmail.com>
2019-09-25 05:44:04 -0400

    Add initial outline of ideas for project

---
+++ spec/spec_psuedocode.rb
@@ -0,0 +1,25 @@
+#will scrape https://www.pcgamer.com/best-pc-games/
+
+#A Game has a title, release date, genre and developer
+
+#A Developer has a name
+
+#A genre has a genre
+
+#Games can have one genre and one developer
+
+#A Developer has many games
+
+#A genre has many games
+
+#A developer has many genres through games
+
+#A genre has many devs through games
+
+#maybe also include link to download/buy? Or reviews?
+
+#add function for pullind desc
+
+#add function for more games like it if possible??
+
+#is able to print all games, devs, genres & associated when they are selected
